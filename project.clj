0f4494c652a4ab63f9a4459c3f0d8120e201817f
Joe Wass <jwass@crossref.org>
2017-06-16 16:58:32 +0100

    Don't log status if there's no endpoint configured.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-common "0.1.26"
+(defproject event-data-common "0.1.27"
   :description "Crossref Event Data Common"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
--- src/event_data_common/status.clj
+++ src/event_data_common/status.clj
@@ -20,65 +20,65 @@
 (defn send!
   "DEPRECATED. Chose add! or replace!."
   [service component fragment heartbeat-count]
-  
-  (let [the-path (str "/status/" service "/" component "/" fragment)]
-    (backoff/try-backoff
-      #(let [result (client/post (str (:status-service env) the-path)
-                         {:headers {"Content-type" "text/plain" "Authorization" (str "Bearer " @jwt-token)}
-                          :body (str heartbeat-count)})]
-           (when-not (= (:status result) 201)
-             (log/error "Can't send status update" the-path "response status:" (:status result))
-             ; Exception caught by try-try-again n times. 
-             (throw (new Exception "Couldn't send status update."))))
-        @wait-delay
-        ; Only try 3 times. If the status service is unavailable, tough luck.
-        ; A lot of data is passed to it, and it's time-sensitive and not mission-critical.
-        ; Unsent heartbeats are a significant signal.
-        3
-        #(log/info "Failed to send status update" (.getMessage %1) "times :" the-path)
-        #(log/info "Gave up sending status update:" the-path)
-        #())))
+  (when-let [base (:status-service env)]
+    (let [the-path (str "/status/" service "/" component "/" fragment)]
+      (backoff/try-backoff
+        #(let [result (client/post (str base the-path)
+                           {:headers {"Content-type" "text/plain" "Authorization" (str "Bearer " @jwt-token)}
+                            :body (str heartbeat-count)})]
+             (when-not (= (:status result) 201)
+               (log/error "Can't send status update" the-path "response status:" (:status result))
+               ; Exception caught by try-try-again n times. 
+               (throw (new Exception "Couldn't send status update."))))
+          @wait-delay
+          ; Only try 3 times. If the status service is unavailable, tough luck.
+          ; A lot of data is passed to it, and it's time-sensitive and not mission-critical.
+          ; Unsent heartbeats are a significant signal.
+          3
+          #(log/info "Failed to send status update" (.getMessage %1) "times :" the-path)
+          #(log/info "Gave up sending status update:" the-path)
+          #()))))
 
 (defn add!
   "Send an update for a component/fragment/heartbeat"
   [service component fragment heartbeat-count]
-  
-  (let [the-path (str "/status/" service "/" component "/" fragment)]
-    (backoff/try-backoff
-      #(let [result (client/post (str (:status-service env) the-path)
-                         {:headers {"Content-type" "text/plain" "Authorization" (str "Bearer " @jwt-token)}
-                          :body (str heartbeat-count)})]
-           (when-not (= (:status result) 201)
-             (log/error "Can't send status update" the-path "response status:" (:status result))
-             ; Exception caught by try-try-again n times. 
-             (throw (new Exception "Couldn't send status update."))))
-        @wait-delay
-        ; Only try 3 times. If the status service is unavailable, tough luck.
-        ; A lot of data is passed to it, and it's time-sensitive and not mission-critical.
-        ; Unsent heartbeats are a significant signal.
-        3
-        #(log/info "Failed to send status update" (.getMessage %1) "times :" the-path)
-        #(log/info "Gave up sending status update:" the-path)
-        #())))
+  (when-let [base (:status-service env)]
+    (let [the-path (str "/status/" service "/" component "/" fragment)]
+      (backoff/try-backoff
+        #(let [result (client/post (str base the-path)
+                           {:headers {"Content-type" "text/plain" "Authorization" (str "Bearer " @jwt-token)}
+                            :body (str heartbeat-count)})]
+             (when-not (= (:status result) 201)
+               (log/error "Can't send status update" the-path "response status:" (:status result))
+               ; Exception caught by try-try-again n times. 
+               (throw (new Exception "Couldn't send status update."))))
+          @wait-delay
+          ; Only try 3 times. If the status service is unavailable, tough luck.
+          ; A lot of data is passed to it, and it's time-sensitive and not mission-critical.
+          ; Unsent heartbeats are a significant signal.
+          3
+          #(log/info "Failed to send status update" (.getMessage %1) "times :" the-path)
+          #(log/info "Gave up sending status update:" the-path)
+          #()))))
 
 (defn replace!
   "Send an update for a component/fragment/heartbeat"
   [service component fragment heartbeat-count]
-  
-  (let [the-path (str "/status/" service "/" component "/" fragment)]
-    (backoff/try-backoff
-      #(let [result (client/put (str (:status-service env) the-path)
-                         {:headers {"Content-type" "text/plain" "Authorization" (str "Bearer " @jwt-token)}
-                          :body (str heartbeat-count)})]
-           (when-not (= (:status result) 201)
-             (log/error "Can't send status update" the-path "response status:" (:status result))
-             ; Exception caught by try-try-again n times. 
-             (throw (new Exception "Couldn't send status update."))))
-        @wait-delay
-        ; Only try 3 times. If the status service is unavailable, tough luck.
-        ; A lot of data is passed to it, and it's time-sensitive and not mission-critical.
-        ; Unsent heartbeats are a significant signal.
-        3
-        #(log/info "Failed to send status update" (.getMessage %1) "times :" the-path)
-        #(log/info "Gave up sending status update:" the-path)
-        #())))
+  (when-let [base (:status-service env)]
+    (let [the-path (str "/status/" service "/" component "/" fragment)]
+      (backoff/try-backoff
+        #(let [result (client/put (str base the-path)
+                           {:headers {"Content-type" "text/plain" "Authorization" (str "Bearer " @jwt-token)}
+                            :body (str heartbeat-count)})]
+             (when-not (= (:status result) 201)
+               (log/error "Can't send status update" the-path "response status:" (:status result))
+               ; Exception caught by try-try-again n times. 
+               (throw (new Exception "Couldn't send status update."))))
+          @wait-delay
+          ; Only try 3 times. If the status service is unavailable, tough luck.
+          ; A lot of data is passed to it, and it's time-sensitive and not mission-critical.
+          ; Unsent heartbeats are a significant signal.
+          3
+          #(log/info "Failed to send status update" (.getMessage %1) "times :" the-path)
+          #(log/info "Gave up sending status update:" the-path)
+          #()))))
