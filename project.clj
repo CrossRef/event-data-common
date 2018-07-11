d6a69f2132c548c8e7fd1fcdcd5b9fb5ea83dffc
Joe Wass <jwass@crossref.org>
2018-07-11 11:10:31 +0100

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject event-data-common "0.1.56"
+(defproject event-data-common "0.1.57"
   :description "Crossref Event Data Common"
   :url "http://eventdata.crossref.org"
   :license {:name "The MIT License (MIT)"
